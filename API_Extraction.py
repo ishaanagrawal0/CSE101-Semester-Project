3d12420cdf5c7c553a2d269e64947937dde088f0
Ishaan Agrawal <113104833+ishaanagrawal0@users.noreply.github.com>
2023-02-15 17:59:23 +0530

    Updated to finalise

--- API_Extraction.py
+++ API_Extraction.py
@@ -2,39 +2,46 @@ import urllib.request
 import json
 import random
 
-with open("my_key.txt", "r") as f:
-    my_key = f.read().rstrip("\n")
+def cardList(player_tag):
 
-    base_url = "https://api.clashroyale.com/v1"
+    with open("my_key.txt", "r") as f:
+        my_key = f.read().rstrip("\n")
 
-    player_tag = "#22VVQLYLV"
+        base_url = "https://api.clashroyale.com/v1"
 
-    endpoint = f"/players/%23{player_tag[1:]}"
+        levels = {}
 
-    request = urllib.request.Request(
-                    base_url+endpoint,
-                    None,
-                    {
-                        "Authorization": "Bearer %s" % my_key
-                    }
-            )
-    response = urllib.request.urlopen(request).read().decode("utf-8")
+        endpoint = f"/players/%23{player_tag[1:]}"
 
-    data = json.loads(response)
-    i = 0
-    while True:
-        print(data["cards"][i]["name"], end = ": ")
-        x = data["cards"][i]["maxLevel"]
-        if x==6:
-            print(data["cards"][i]["level"] + 8)
-        elif x==4:
-            print(data["cards"][i]["level"] + 10)
-        elif x==9:
-            print(data["cards"][i]["level"] + 5)
-        elif x==12:
-            print(data["cards"][i]["level"] + 2)
-        else:
-            print(data["cards"][i]["level"])
-        i += 1
-        #print(data["cards"]["level"])
-        #print("Name: %s\nTrophies: %d\nArena: %s\nTag: %s\n\n" % (item["name"], item["trophies"], item["arena"], item["tag"]))
+        request = urllib.request.Request(
+                        base_url+endpoint,
+                        None,
+                        {
+                            "Authorization": "Bearer %s" % my_key
+                        }
+                )
+        response = urllib.request.urlopen(request).read().decode("utf-8")
+
+        data = json.loads(response)
+        i = 0
+        while True:
+            try:
+                x = data["cards"][i]["maxLevel"]
+                if x==6:
+                    levels[data["cards"][i]["name"]] = data["cards"][i]["level"] + 8
+                elif x==4:
+                    levels[data["cards"][i]["name"]] = data["cards"][i]["level"] + 10
+                elif x==9:
+                    levels[data["cards"][i]["name"]] = data["cards"][i]["level"] + 5
+                elif x==12:
+                    levels[data["cards"][i]["name"]] = data["cards"][i]["level"] + 2
+                else:
+                    levels[data["cards"][i]["name"]] = data["cards"][i]["level"]
+
+                i += 1
+            except:
+                return levels
+            #print(data["cards"]["level"])
+            #print("Name: %s\nTrophies: %d\nArena: %s\nTag: %s\n\n" % (item["name"], item["trophies"], item["arena"], item["tag"]))
+
+print(cardList("#22VVQLYLV"))
