047378e77c2d56d768908f1455b9bc97fd1ffe42
critz㌹ <pokemonmasterswapnil@gmail.com>
2023-02-16 10:10:06 +0530

    Tourney Bracket done (in a scuffed way, yes)

--- Tourney.py
+++ Tourney.py
@@ -1,2 +1,8 @@
 import API_Extraction as api
 
+cards=[]
+with open("Tourney.txt") as F:
+    player=F.readline().split()
+    cards.append(api.cardList(player))
+#print(api.cardList("#22VVQLYLV"))
+print(api.cardList("#YCRYQV8"))
