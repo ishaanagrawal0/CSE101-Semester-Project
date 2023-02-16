e83fd60b5fef9bb8706d4f70c14073472e3bb705
critz㌹ <pokemonmasterswapnil@gmail.com>
2023-02-16 08:08:06 +0530

    Started on Tourney file

--- API_Extraction.py
+++ API_Extraction.py
@@ -6,36 +6,12 @@ def cardList(player_tag):
     with open("my_key.txt", "r") as f:
         my_key = f.read().rstrip("\n")
 
-<<<<<<< HEAD
-    player_tag = "#2LPQC098"
-=======
         base_url = "https://api.clashroyale.com/v1"
->>>>>>> 2efcdf853573516ffb53bfd1f7f1886e90b45085
 
         levels = {}
 
         endpoint = f"/players/%23{player_tag[1:]}"
 
-<<<<<<< HEAD
-    data = json.loads(response)
-    i = 0
-    while True:
-        print(data["cards"][i]["name"], end = ": ")
-        x = data["cards"][i]["maxLevel"]
-        if x==6:
-            print(data["cards"][i]["level"] + 8)
-        elif x==4:
-            print(data["cards"][i]["level"] + 10)
-        elif x==9:
-            print(data["cards"][i]["level"] + 5)
-        elif x==12:
-            print(data["cards"][i]["level"] + 2)
-        else:
-            print(data["cards"][i]["level"])
-        i += 1
-        print(data["cards"]["level"])
-        print("Name: %s\nTrophies: %d\nArena: %s\nTag: %s\n\n" % (item["name"], item["trophies"], item["arena"], item["tag"]))
-=======
         request = urllib.request.Request(
                         base_url+endpoint,
                         None,
@@ -66,6 +42,3 @@ def cardList(player_tag):
                 return levels
             #print(data["cards"]["level"])
             #print("Name: %s\nTrophies: %d\nArena: %s\nTag: %s\n\n" % (item["name"], item["trophies"], item["arena"], item["tag"]))
-
-print(cardList("#22VVQLYLV"))
->>>>>>> 2efcdf853573516ffb53bfd1f7f1886e90b45085
